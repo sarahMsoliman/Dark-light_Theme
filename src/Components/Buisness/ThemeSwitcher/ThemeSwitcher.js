d40e3c82c94e0c7100e6c7844d8d0ca0404b9048
unknown <sarah.mohamed.soliman@gmail.com>
2019-06-19 10:07:29 +0200

    Added app's layout.

---
+++ src/Components/Buisness/ThemeSwitcher/ThemeSwitcher.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ThemeSwitcher extends Component {
+    render () {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ThemeSwitcher
---
+++ src/Layouts/ListsLayout/ListsLayout.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class ListsLayout extends Component {
+    render () {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default ListsLayout
