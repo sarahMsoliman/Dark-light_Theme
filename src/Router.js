6dafedeaaa8af9bac949923b6e63e9a315b8e6fc
unknown <sarah.mohamed.soliman@gmail.com>
2019-06-22 23:44:08 +0200

    Added navbar styles

--- src/Router.js
+++ src/Router.js
@@ -4,12 +4,14 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import HomeLayout from './Layouts/HomeLayout/HomeLayout';
 import SettingsLayout from './Layouts/SettingsLayout/SettingsLayout';
 import ListsLayout from './Layouts/ListsLayout/ListsLayout';
+import Header from './Components/Reusable/Header/Header'
 
 class Router extends Component {
     render() {
         return (
             <BrowserRouter>
                 <React.Fragment>
+                    <Header/>
                     <Switch>
                         <Route path="/home" render={()=><HomeLayout />} exact />
                         <Route path="/settings" render={() => <SettingsLayout />} exact />
