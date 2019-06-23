2de91f2100dd2ed6b75008e64990cd49ef81768d
Sarah Soliman <sarah.mohamed.soliman@gmail.com>
2019-06-23 15:18:10 +0200

    Added logo image to the header

--- src/Components/Reusable/Header/Header.js
+++ src/Components/Reusable/Header/Header.js
@@ -1,26 +1,40 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import './Header.css'
 
 class Header extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
-                <a className="navbar-brand" href="#logo">Navbar</a>
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav mr-auto">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#apps">Our Apps <span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#mobile">Mobile App <span className="sr-only">(current)</span></a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        )
-    }
+  render() {
+    return (
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        {/* <a className="navbar-brand" href="#logo">Navbar</a> */}
+        <img src={process.env.PUBLIC_URL + "/Images/logo.png"} alt="logo" className="logo"/>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="#apps">
+                Our Apps <span className="sr-only">(current)</span>
+              </a>
+            </li>
+            <li className="nav-item active">
+              <a className="nav-link" href="#mobile">
+                Mobile App <span className="sr-only">(current)</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
-export default Header
+export default Header;
